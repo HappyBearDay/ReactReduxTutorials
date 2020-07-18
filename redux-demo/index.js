cdd751b0a4534898ed3801c530b0bc3ed0205121
happybearday <anishamroun2@gmail.com>
2020-07-18 15:05:39 +0200

    React Redux Tutorials - 6 - Reducers

--- redux-demo/index.js
+++ redux-demo/index.js
@@ -11,3 +11,26 @@ function buyCake(){
         info: 'First redux action'
     }
 }
+
+
+// State
+
+const initialState = {
+    numOfCakes: 10
+}
+
+
+// Reducer
+
+//(previousState, action) => newState
+
+const reducer = (state = initialState, action)=>{
+    switch(action.type){
+        case BUY_CAKE: return {
+            ...state,
+            numOfCakes : state.numOfCakes -1 
+        }
+    }
+}
+
+
