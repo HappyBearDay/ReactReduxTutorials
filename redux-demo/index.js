2784fc10a0403ac62b1db01f9a8d7254da79c86b
happybearday <anishamroun2@gmail.com>
2020-07-18 17:34:46 +0200

    React Redux Tutorials - 7 - Store

--- redux-demo/index.js
+++ redux-demo/index.js
@@ -1,9 +1,8 @@
+const redux = require('redux')
+const createStore = redux.createStore
 
 // Action
-
 const BUY_CAKE = "BUY_CAKE"
-
-
 
 function buyCake(){
     return {
@@ -12,25 +11,26 @@ function buyCake(){
     }
 }
 
-
 // State
-
 const initialState = {
     numOfCakes: 10
 }
 
-
 // Reducer
-
-//(previousState, action) => newState
-
-const reducer = (state = initialState, action)=>{
-    switch(action.type){
-        case BUY_CAKE: return {
-            ...state,
-            numOfCakes : state.numOfCakes -1 
-        }
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+      case BUY_CAKE: return {
+          ...state,
+        numOfCakes: state.numOfCakes - 1
+      }
+      default: return state
     }
-}
+  }
 
-
+const store = createStore(reducer)
+console.log('Initial State ', store.getState())
+const unsubscribe = store.subscribe(() => console.log('Updated State ', store.getState()))
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+unsubscribe()
