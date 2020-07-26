48cf61dca5cef752e3680fbe533a3edf91721681
happybearday <anishamroun2@gmail.com>
2020-07-27 00:19:12 +0200

    modify project organization

--- react-redux-demo/src/App.js
+++ react-redux-demo/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Provider} from "react-redux"
-import store from "./components/redux/store"
+import store from "./redux/store"
 import './App.css';
 import CakeContainer from './components/CakeContainer'
 function App() {
