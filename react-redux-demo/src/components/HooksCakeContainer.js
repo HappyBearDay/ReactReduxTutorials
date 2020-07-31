1cbce26764afd4c06dc7d69dc35b5958da7c5a27
happybearday <anishamroun2@gmail.com>
2020-07-31 20:15:07 +0200

    React Redux Tutorials - 22 - Cakes and Ice Creams

--- react-redux-demo/src/components/HooksCakeContainer.js
+++ react-redux-demo/src/components/HooksCakeContainer.js
@@ -4,7 +4,7 @@ import { buyCake } from "../redux"
 
 
 function HookCakeContainer(){
-    const numberOfCakes = useSelector(state => state.numberOfCakes)
+    const numberOfCakes = useSelector(state => state.cake.numberOfCakes)
     const dispatch = useDispatch()
     return (
         <div>
